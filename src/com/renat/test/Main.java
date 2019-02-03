814c9bce36d644c9a3018c8217a33499b346f8b4
lacoste89 <Microsoft32>
2019-02-03 19:31:59 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/renat/test/Main.java

--- src/com/renat/test/Main.java
+++ src/com/renat/test/Main.java
@@ -1,10 +1,17 @@
 package com.renat.test;
 
 public class Main {
+
+    public static  <T> T getT(Object obj){
+
+        return (T) obj;
+    }
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println("Hello word");
         System.out.println("Hello word");
-
+        System.out.println(Main.<Integer>getT(new Object()).getClass().toString());
     }
+
+
 }
